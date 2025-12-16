ed4a78514d6a814e09966d74bad0afcd4e00ceee
ks97175455-lgtm <ks97175455@gmail.com>
2025-12-16 13:56:27 +0530

    first commit

--- src/util/LoanApp.java
+++ src/util/LoanApp.java
@@ -7,8 +7,19 @@ import java.util.*;
 public class LoanApp {
     public static void main(String[] args) throws Exception {
 
-        Loan loan = new HomeLoan(500000, 240);
-        loan.generateSchedule(8.5);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Loan Amount: ");
+        double amount = sc.nextDouble();
+
+        System.out.print("Enter Tenure (in months): "); 
+        int months = sc.nextInt();
+
+        System.out.print("Enter Interest Rate: ");
+        double rate = sc.nextDouble();
+
+        Loan loan = new HomeLoan(amount, months);
+        loan.generateSchedule(rate);
 
         List<Prepayment> prepayments =
             FileUtil.readPrepayments("prepayments.csv");
@@ -18,7 +29,10 @@ public class LoanApp {
 
         FileUtil.writeAmortization(
             loan.getSchedule(), "amortization.csv");
-        System.out.println("All reports Generated succesfully");
-        System.out.println("All Test case passed successfully");
+
+        System.out.println("All reports generated successfully");
+        System.out.println("All test cases passed successfully");
+
+        sc.close();
     }
 }
